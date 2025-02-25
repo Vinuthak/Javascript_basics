d2aa625f90faf7c95ee962ea59374fd9a12067bb
Vinuthak <vinutha.kasaragod@gmail.com>
2025-02-25 23:09:34 +0100

    Add HTML page by looping through the data.

--- scripts/11-todo-list.js
+++ scripts/11-todo-list.js
@@ -1,11 +1,24 @@
-const todoList = [];
+const todoList = ['make dinner', 'wash dishes', 'watch news'];
+ renderTodoList();
+function renderTodoList(){
+  let todoListHTML = '';
+  for(i = 0;i < todoList.length;i++){
+    const todo = todoList[i];
+    const html = ` <p>${todo}</p> `
+    todoListHTML += html; 
+  }
+  document.querySelector('.js-todo-list').innerHTML = todoListHTML;
+  console.log(todoListHTML);
+}
 
 function addList(){
   const inputElement = document.querySelector('.js-name-input');
   const todoName = inputElement.value
-  console.log(`My List is ${todoName}`);
+  //console.log(`My List is ${todoName}`);
   todoList.push(todoName);
   console.log(todoList);
 
   inputElement.value = '';
+  renderTodoList();
 }
+  
