e2a53f53226823cfb1e2a3affabcbb19e837598c
Vinuthak <vinutha.kasaragod@gmail.com>
2025-03-03 22:25:50 +0100

    add playing with keyboard keys 'r','p','s'

--- scripts/12.rock-paper-scissors .js
+++ scripts/12.rock-paper-scissors .js
@@ -35,6 +35,16 @@ rockButton.addEventListener('click',()=> {playGame('rock')});
 document.querySelector('.js-paper-button').addEventListener('click',()=>{playGame('paper')});
 document.querySelector('.js-scissors-button').addEventListener('click',()=>{playGame('scissors')});
 
+document.body.addEventListener('keydown', (event)=>{
+  if(event.key === 'r'){
+    playGame('rock');
+  }else if(event.key === 'p'){
+    playGame('paper');
+  }else if(event.key ==='s'){
+    playGame('scissors');
+  }
+});
+
 function playGame(playerMove){
 
   const computerMove = pickComputerMove();
